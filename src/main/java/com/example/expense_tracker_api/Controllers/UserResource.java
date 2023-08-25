a7c29ad8b7fe1809ebf22eab378f2960addb4cc1
bharath <bsbharath2019@gmail.com>
2023-08-25 19:16:35 +0530

    adding all the files into git

---
+++ src/main/java/com/example/expense_tracker_api/Controllers/UserResource.java
@@ -0,0 +1,23 @@
+package com.example.expense_tracker_api.Controllers;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserResource {
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody Map<String,Object> userMap){
+        String firstName=userMap.get("firstName").toString();
+        String lastName=userMap.get("lastName").toString();
+        String email=userMap.get("email").toString();
+        String password=userMap.get("password").toString();
+        return firstName+" "+lastName+" "+email+" "+password;
+    }
+}
